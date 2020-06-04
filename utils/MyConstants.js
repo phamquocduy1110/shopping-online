61d98be08a066ac2d607cee07f941eb3102e4a4f
Sakura Ayame Kinomoto <55733660+phamquocduy1110@users.noreply.github.com>
2020-06-04 14:31:55 +0700

    Initial commit

---
+++ utils/MyConstants.js
@@ -0,0 +1,10 @@
+var MyConstants = {
+  DB_SERVER: 'mongodbdemo-fzcml.mongodb.net',
+  DB_USER: 'User',
+  DB_PASS: '123',
+  DB_DATABASE: 'SO',
+  HOSTNAME: 'localhost:3000',
+  EMAIL_USER: 'duy.187pm13901@vanlanguni.vn',
+  EMAIL_PASS: 'Phamquocduy*1110',
+};
+module.exports = MyConstants;
